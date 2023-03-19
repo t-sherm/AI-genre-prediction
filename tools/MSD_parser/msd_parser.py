6e1d993e4f4e82981859d563426aee19b505fafa
Joshua Canlas <canlas.joshua0121@gmail.com>
2023-03-19 13:54:29 -0700

    able to extract desired features except for segment pitches

--- tools/MSD_parser/msd_parser.py
+++ tools/MSD_parser/msd_parser.py
@@ -17,8 +17,8 @@ import sys
 import time
 import glob
 import datetime
-import sqlite3
 import numpy as np
+import pandas as pd
 
 
 # path to the Million Song Dataset data (uncompressed)
@@ -26,7 +26,6 @@ msd_subset_data_path='data/MillionSongSubset'
 assert os.path.isdir(msd_subset_data_path),'wrong path' # sanity check
 # path to the Million Song Dataset code
 msd_code_path='tools/MSD_python_code'
-import pdb; pdb.set_trace()
 assert os.path.isdir(msd_code_path),'wrong path' # sanity check
 # add tools to Python path
 sys.path.append( msd_code_path)
@@ -35,17 +34,21 @@ sys.path.append( msd_code_path)
 import hdf5_getters as GETTERS
 
 
-def strtimedelta(starttime,stoptime):
-    """
-    The following function simply gives us a nice string for
-    a time lag in seconds.
-    INPUT
-        starttime
-        stoptime
-    OUTPUT
-        time difference (sec)
-    """
-    return str(datetime.timedelta(seconds=stoptime-starttime))
+# Need to dictionary to house all of the desired features globally
+desired_features = {'Artist Name': [],
+                    'Release': [],
+                    'Song Title': [],
+                    'Duration (sec)': [],
+                    'Key': [],
+                    'Key Confidence': [],
+                    'Mode': [],
+                    'Mode Confidence': [],
+                    'Loudness': [],
+                    'Tempo': [],
+                    'Time Signature': [],
+                    'Time Signature Confidence': [],
+                    'End of Fade In': []}
+
 
 def apply_to_all_files(basedir,func=lambda x: x,ext='.h5'):
     """
@@ -71,35 +74,60 @@ def apply_to_all_files(basedir,func=lambda x: x,ext='.h5'):
             func(f)       
     return cnt
 
-def func_to_get_artist_name(filename):
+def func_to_get_desired_features(filename):
     """
     This function does 3 simple things:
     - open the song file
-    - get artist ID and put it
+    - get artist name and put it in a pandas dataframe
     - close the file
     """
     h5 = GETTERS.open_h5_file_read(filename)
+
     artist_name = GETTERS.get_artist_name(h5)
-    all_artist_names.add( artist_name )
+    release = GETTERS.get_release(h5)
+    title = GETTERS.get_title(h5)
+    duration = GETTERS.get_duration(h5)
+    key = GETTERS.get_key(h5)
+    key_confidence = GETTERS.get_key_confidence(h5)
+    mode = GETTERS.get_mode(h5)
+    mode_confidence = GETTERS.get_mode_confidence(h5)
+    loudness = GETTERS.get_loudness(h5)
+    tempo = GETTERS.get_tempo(h5)
+    time_signature = GETTERS.get_time_signature(h5)
+    time_signature_confidence = GETTERS.get_time_signature_confidence(h5)
+    end_of_fade_in = GETTERS.get_end_of_fade_in(h5)
+
+    desired_features['Artist Name'].append(artist_name.decode('UTF-8'))
+    desired_features['Release'].append(release.decode('UTF-8'))
+    desired_features['Song Title'].append(title.decode('UTF-8'))
+    desired_features['Duration (sec)'].append(duration)
+    desired_features['Key'].append(key)
+    desired_features['Key Confidence'].append(key_confidence)
+    desired_features['Mode'].append(mode)
+    desired_features['Mode Confidence'].append(mode_confidence)
+    desired_features['Loudness'].append(loudness)
+    desired_features['Tempo'].append(tempo)
+    desired_features['Time Signature'].append(time_signature)
+    desired_features['Time Signature Confidence'].append(time_signature_confidence)
+    desired_features['End of Fade In'].append(end_of_fade_in)
+
     h5.close()
 
 
 if __name__ == '__main__':
     # we can now easily count the number of files in the dataset
-    print('number of song files:',apply_to_all_files(msd_subset_data_path))
+    num_song_files = apply_to_all_files(msd_subset_data_path)
+    print('number of song files:', num_song_files)
 
-    # let's now get all artist names in a set(). One nice property:
-    # if we enter many times the same artist, only one will be kept.
-    all_artist_names = set()
-        
-    # let's apply the previous function to all files
-    # we'll also measure how long it takes
-    t1 = time.time()
-    apply_to_all_files(msd_subset_data_path,func=func_to_get_artist_name)
-    t2 = time.time()
-    print('all artist names extracted in:',strtimedelta(t1,t2))
+    # Extract desired features and store them in a dictionary
+    print('Extracting desired features...')
+    apply_to_all_files(msd_subset_data_path, func=func_to_get_desired_features)
 
-    # let's see some of the content of 'all_artist_names'
-    print('found',len(all_artist_names),'unique artist names')
-    for k in range(5):
-        print(list(all_artist_names)[k])
+    # Create pandas dataframe to house desired features
+    msd_dataframe = pd.DataFrame(desired_features)
+    
+    # Export pandas dataframe to csv file
+    print('Exporting to csv file...')
+    msd_dataframe.to_csv('data_processed/MSD_Desired_Features.csv')
+
+    
