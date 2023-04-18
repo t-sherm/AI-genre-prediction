951f4984a9c77f3b42bbd6cb30ee4ef62d23444b
Joshua Canlas <canlas.joshua0121@gmail.com>
2023-04-17 21:18:14 -0700

    made new test and training data csv files with new desired features

--- algorithms/random_forest.py
+++ algorithms/random_forest.py
@@ -6,10 +6,13 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Load data from csv
-assert os.path.isfile('data_processed/test_data.csv'), 'file does not exist' # sanity check
-assert os.path.isfile('data_processed/training_data.csv'), 'file does not exist' # sanity check
-test_data_file = glob.glob('data_processed/test_data.csv')
-train_data_file = glob.glob('data_processed/training_data.csv')
+assert os.path.isfile('data_processed/test_data_NEW.csv'), 'file does not exist' # sanity check
+assert os.path.isfile('data_processed/training_data_NEW.csv'), 'file does not exist' # sanity check
+test_data_file = glob.glob('data_processed/test_data_NEW.csv')
+train_data_file = glob.glob('data_processed/training_data_NEW.csv')
 
-train_data = pd.read_csv(train_data_file)
-test_data = pd.read_csv(test_data_file)
+import pdb; pdb.set_trace()
+
+train_data = pd.read_csv(train_data_file[0])
+test_data = pd.read_csv(test_data_file[0])
+
